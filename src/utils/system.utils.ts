b8eb746360aa7bce2d5ced6654664e4b46d09681
Mike AUBENAS <mike.aubenas@gmail.com>
2023-03-01 01:59:42 +0100

    feat(script): file stats for prod log file

--- src/utils/system.utils.ts
+++ src/utils/system.utils.ts
@@ -1,3 +1,7 @@
+import { exec } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
 export type NodeEnv = 'dev' | 'production';
 
 export class SystemUtils {
@@ -7,4 +11,24 @@ export class SystemUtils {
   static exit = (code = 0) => process.exit(code);
 
   static getEnv = () => process.env.NODE_ENV as NodeEnv;
+
+  static shell = (
+    command: string,
+  ): Promise<{ stdout?: string; stderr?: string }> => {
+    return new Promise((resolve, reject) => {
+      exec(command, (error, stdout, stderr) => {
+        if (error) reject(error);
+
+        stdout = stdout || undefined;
+        stderr = stderr || undefined;
+        resolve({ stdout, stderr });
+      });
+    });
+  };
+
+  static getAbsolutePath = (filePath: string) =>
+    path.isAbsolute(filePath) ? filePath : path.join(process.cwd(), filePath);
+
+  static doesFileExists = (relativePath: string): boolean =>
+    fs.existsSync(this.getAbsolutePath(relativePath)) || false;
 }
